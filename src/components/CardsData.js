8aabaf48624e3ce1633890db836e0a233ec81023
Usman-99 <112505379+Usman-99@users.noreply.github.com>
2022-10-11 21:44:54 +0500

    Update CardsData.js

--- src/components/CardsData.js
+++ src/components/CardsData.js
@@ -1,27 +1,33 @@
+import glasses1 from"../Images/man1.png"
+import glasses2 from"../Images/man2.jpg"
+import glasses3 from"../Images/man3.jpg"
+import glasses4 from"../Images/man4.jpg"
+import glasses5 from"../Images/man5.jpg"
+import glasses6 from"../Images/man6.jpg"
 const manCardsData= [{
     category: "Man",
     title: "Blackberry",
-    img: "https://via.placeholder.com/400",price:1500
+    img: glasses1,price:1500
 }, {
     category: "Man",
     title: "Blueberry",
-    img: "https://via.placeholder.com/400",price:1500
+    img: glasses2,price:1500
 }, {
     category: "Man",
     title: "Whiteberry",
-    img: "https://via.placeholder.com/400",price:1500
+    img: glasses3,price:1500
 }, {
     category: "Man",
     title: "UniqueBLACK",
-    img: "https://via.placeholder.com/400",price:1500
+    img:glasses4,price:1500
 }, {
     category: "Man",
     title: "UniqueBLUE",
-    img: "https://via.placeholder.com/400",price:1500
+    img: glasses6,price:1500
 }, {
     category: "Man",
     title: "UniqueWHITE",
-    img: "https://via.placeholder.com/400",price:1500
+    img: glasses5,price:1500
 }, {
     category: "Man",
     title: "OrientNEW",
