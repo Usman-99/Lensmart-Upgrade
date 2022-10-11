421cf9efd574c94fff897ea632e4e941a6ee31ab
Usman-99 <112505379+Usman-99@users.noreply.github.com>
2022-10-11 21:49:19 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,8 @@ export default function Footer() {
     return (
         <footer className="w3-center w3-black w3-padding-64">
             <button className="btn btn-success" onClick={() => {
-                navigate("/");
                 window.scrollTo(0,0)
-            }}><i className="fa fa-arrow-up w3-margin-right" ></i>Go to Home</button>
+            }}><i className="fa fa-arrow-up w3-margin-right" ></i>Go to Top</button>
             <div className="w3-xlarge w3-section">
                 <i className="fa-brands fa-facebook-official w3-hover-opacity me-1"></i>
                 <i className="fa-brands fa-instagram w3-hover-opacity me-1"></i>
@@ -15,7 +14,10 @@ export default function Footer() {
             </div>
             <p className="w3-large text-info">This website is made by Muhammad Usman Yousuf <b className="px-3">&copy;</b></p>
             <br />
-            <button className="btn btn-light  btn-small  w3-margin-bottom"><b>Buy now</b></button>
+            <button className="btn btn-light border-info  btn-small  w3-margin-bottom" style={{border:"4px solid"}} onClick={()=>{
+                navigate("/EyeGlasses")
+                window.scrollTo(0,0)
+            }}><b>Buy now</b></button>
         </footer>
     )
 }
